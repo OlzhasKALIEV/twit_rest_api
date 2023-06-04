f1693b57d789037d9e2f47c7baaff041ae77419c
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-06-05 00:11:43 +0600

    04/06/2023 - Twit(self.like)

--- model/twit.py
+++ model/twit.py
@@ -2,6 +2,7 @@ from model.user import User
 
 
 class Twit:
-    def __init__(self, body: str, author: User):
+    def __init__(self, body: str, author: User, like: int):  # (like: 1 - лайк, 0 - дизлайк)
         self.body = body
         self.author = author
+        self.like = like
